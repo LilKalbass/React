53f67c298a25fccfb20868f7d2b364c58d32e07e
Artem Dimoglov <adimoglov@gmail.com>
2022-07-10 08:34:50 +0300

    dd

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Example from "./Example";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      country: "Ukraine"
+    }
+  }
+
+  changeText = () =>{
+    this.state.country === "Ukraine" ? this.setState({country:"Ukraine is the best"}) : this.setState({country:"Ukraine"});
+
+  }
+
+  render() {
+    return (
+        <>
+          <Example data={this.state.country} />
+          <input type="text" placeholder="s"/>
+          <button onClick={this.changeText}>Поменять текст</button>
+        </>
+    )
+  }
 }
 
 export default App;
