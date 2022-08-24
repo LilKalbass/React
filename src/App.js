e24f47ed0f5316e3d45bae170f4e2f31eb7ab57e
Artem Dimoglov <adimoglov@gmail.com>
2022-08-24 04:35:25 +0300

    Created an ability to delete post

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+    postsArr: []
+    }
+  }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(response => response.json())
+            .then(json => this.setState({postsArr: json}))
+    }
+
+    removePost(i) {
+        const newArr = this.state.postsArr.map(item => {
+            const obj = item;
+            if (item.id > i) {
+                obj.id--;
+            }
+            return obj
+        });
+        newArr.splice(i, 1);
+        this.setState({ postsArr: newArr })
+    }
+
+  render() {
+    console.log(this.state);
+    return(
+        <div className="container">
+            <div className="buttons">
+                {/*<button className = "buttonAdd">Add post</button>*/}
+                {/*<button className = "buttonDelete">Delete list</button>*/}
+                {/*<button className = "buttonChange">Change post</button>*/}
+            </div>
+            <ul>
+                {this.state.postsArr.map((post, i) =>
+                    <li key={i}>
+                        <p>{post.id}</p>
+                        <h5 >{post.title}</h5>
+                        <p>{post.body}</p>
+                        <button className = "buttonRemove" onClick={() => this.removePost(i)}>Remove post</button>
+                    </li>
+                )}
+            </ul>
+        </div>
+    )
+  }
 }
 
 export default App;
