e31745dba83e2ba1e042e13ac3ed57ce90ed78cf
Artem Dimoglov <adimoglov@gmail.com>
2022-08-24 06:39:36 +0300

    created 'change post button'

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-    postsArr: []
+        postsArr: [],
+        newPost: "",
     }
   }
 
@@ -25,27 +26,43 @@ class App extends React.Component {
         this.setState({postsArr: this.state.postsArr})
     }
 
-    createPost() {
+    handleChange = (event) => {
+      this.setState({[event.target.name]: event.target.value});
+    }
 
+    changePost(i) {
+        const post = this.state.postsArr[i];
+        post.body = this.state.newPost;
+
+        this.setState({newPost: "",})
     }
 
   render() {
     console.log(this.state);
     return(
         <>
-            <form onSubmit={this.createPost} className="buttons">
-                <input type="text"/>
-                <button type="submit" className = "buttonAdd">Add post</button>
-                {/*<button className = "buttonDelete">Delete list</button>*/}
-                {/*<button className = "buttonChange">Change post</button>*/}
-            </form>
-            <ul>
+            {/*/!*<form onSubmit = {this.createPost} className = "buttons">*!/*/}
+            {/*    <input type = "text"/>*/}
+            {/*    <button type = "submit" className = "buttonAdd">Add post</button>*/}
+            {/*    <button className = "buttonDelete" >Delete list</button>*/}
+            {/*    <button className = "buttonChange">Change post</button>*/}
+            {/*/!*</form>*!/*/}
+            <ul className = "list">
                 {this.state.postsArr.map((post, i) =>
-                    <li key={i}>
-                        <p>{post.id}</p>
-                        <h5 >{post.title}</h5>
+                    <li className = "list-item" key={i}>{post.id}
+                        <h4 >{post.title}</h4>
                         <p>{post.body}</p>
-                        <button className = "buttonRemove" onClick={() => this.removePost(i)}>Remove post</button>
+                        <input
+                            className = "post"
+                            name = "newPost"
+                            type = "text"
+                            value={this.state.newPost}
+                            onChange={this.handleChange}
+                        />
+                        <div className = "buttons">
+                            <button onClick = {() => this.changePost(i)}>Change post</button>
+                            <button onClick = {() => this.removePost(i)}>Remove post</button>
+                        </div>
                     </li>
                 )}
             </ul>
